e96e927d73fd60c3112e5da514be860046ba50b5
KevCareSA <kev.mkbn2@gmail.com>
2024-03-21 18:22:00 +0200

    database functionality file

---
+++ create_db.py
@@ -0,0 +1,12 @@
+import sqlite3
+import os
+
+def create_db():
+    conn = sqlite3.connect(database=r'inventory.db')
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS employee (eid INTEGER PRIMARY KEY AUTOINCREMENT, name text, email text, gender text, contact text, dob text, doj text, pass text, address text, salary)")
+    conn.commit()
+    
+   
+
+create_db()
